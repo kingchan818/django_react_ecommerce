2fb09aa2914f3b63029ff61fbdad61c6e7f152ca
calvin <chancch70@gmail.com>
2021-04-28 20:31:39 +0800

    url's and views CleanUp

--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render
-from django.http import JsonResponse
-
 from rest_framework.decorators import api_view,permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
-from .products import products
-from .models import *
-from .serializer import *
+from base.models import *
+from base.serializer import *
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -29,28 +26,9 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
-@api_view(['GET'])
-def getRoute(request):
-    routes = [
-        '/api/products/',
-        '/api/products/create/',
-        '/api/products/upload/',
-
-        '/api/products/<id>/reviews/',
-
-        '/api/products/top/',
-        '/api/products/<id>/',
-
-        '/api/products/delete/<id>',
-        '/api/products/<update/<id>',
-    ]
-    return Response(routes)
-
-
 @api_view(['POST'])
 def registerUser(request):
     data = request.data
-
     try:
         user = User.objects.create(
             first_name= data['name'],
@@ -64,21 +42,6 @@ def registerUser(request):
     except:
         message = {'detial' : 'Sorry this email has already exist'}
         return Response(message, status=status.HTTP_400_BAD_REQUEST)
-
-
-
-@api_view(['GET'])
-def getProducts(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products,many=True)
-    return Response(serializer.data)
-
-
-@api_view(['GET'])
-def getProduct(request,pk):
-    product = Product.objects.get(_id=pk)
-    serializer= ProductSerializer(product,many=False )
-    return Response(serializer.data)
 
 
 @api_view(['GET'])
